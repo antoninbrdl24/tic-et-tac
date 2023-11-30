f0f7b06a2330d003d0c4988d24ec5e8a71f49739
Laura WOLFF <laura.wolff@live.fr>
2023-11-30 16:06:39 +0100

    t as vu mes noix?

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SquirellRepository;
+use App\Repository\NutRepository;
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SquirellRepository $squirellRepository): Response
+    public function index(SquirellRepository $squirellRepository, NutRepository $nutRepository): Response
     {   
         $squirells = $squirellRepository->findAll();
-        return $this->render('home/index.html.twig',['squirells'=>$squirells]);
+        $nuts = $nutRepository->findAll();
+        return $this->render('home/index.html.twig',['squirells'=>$squirells, 'nuts' => $nuts]);
     }
 }
